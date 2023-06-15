289ebd141c6ab63ecc0bb630c3c08d79c26b356c
a <a@a>
2023-06-15 14:32:06 -0300

    resolvendo bugs do xml e cmake

--- app/src/main/java/com/example/module/MainActivity.kt
+++ app/src/main/java/com/example/module/MainActivity.kt
@@ -2,23 +2,16 @@ package com.example.module
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
-
 import kotlinx.android.synthetic.main.all_projects_file.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var projectCardAdapter: projectCardAdapter
-    /*private lateinit var binding: ActivityMainBinding*/
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.all_projects_file)
-
-        /*binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)*/
 
         initRecycleView()
         addAllProjectsDatasource()
